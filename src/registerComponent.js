4d5fa7f26b19edb373cc8e085b0824431c6303b8
END-i <end-i@iMac-END-i.local>
2019-04-18 12:48:28 +0300

    f

--- src/registerComponent.js
+++ src/registerComponent.js
@@ -1,27 +1,21 @@
-import { Navigation } from 'react-native-navigation'
-import { Provider } from 'react-redux'
+import { Navigation } from "react-native-navigation";
+import { Provider } from "react-redux";
 
-import ProductsScreen from './screens/ProductsScreen'
-import DetailsScreen from './screens/DetailsScreen'
-import CartScreen from './screens/CartScreen'
-import SideMenuScreen from './screens/SideMenuScreen'
-import TopBar from './screens/TopBar'
-import RightBtn from './screens/RightBtn'
+import ProductsScreen from "./screens/ProductsScreen";
+import DetailsScreen from "./screens/DetailsScreen";
+import CartScreen from "./screens/CartScreen";
+import SideMenuScreen from "./screens/SideMenuScreen";
 
-
-import store from './store'
+import store from "./store";
 
 export function registerComponent() {
   Navigation.registerComponentWithRedux(
-    'Products',
+    "Products",
     () => ProductsScreen,
     Provider,
     store
-  )
-  Navigation.registerComponent('Details', () => DetailsScreen)
-  Navigation.registerComponent('Cart', () => CartScreen)
-  Navigation.registerComponent('SideMenu', () => SideMenuScreen)
-  Navigation.registerComponent('TopBar', () => TopBar)
-  Navigation.registerComponent('RightBtn', () => RightBtn)
-
+  );
+  Navigation.registerComponent("Details", () => DetailsScreen);
+  Navigation.registerComponent("Cart", () => CartScreen);
+  Navigation.registerComponent("SideMenu", () => SideMenuScreen);
 }
